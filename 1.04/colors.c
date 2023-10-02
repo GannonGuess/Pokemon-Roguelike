0d4d2a8604b3f4500edcc521904b76b596e7adac
GannonGuess <gannonkguess@gmail.com>
2023-10-01 21:35:00 -0500

    Created V1.04
    * Copied files from 1.03 to 1.04
    * Updated README and CHANGELOG with 1.04

---
+++ 1.04/colors.c
@@ -0,0 +1,32 @@
+//File for colorizing output
+void blue() {
+    printf("\e[1;34m");
+}
+
+void black() {
+    printf("\e[1;30m");
+}
+
+void cyan() {
+    printf("\e[0;36m");
+}
+
+void yellow() {
+    printf("\e[0;33m");
+}
+
+void green() {
+    printf("\e[0;32m");
+}
+
+void white() {
+    printf("\x1b[m");
+}
+
+void red() {
+    printf("\e[0;31m");
+}
+
+void purple() {
+    printf("\e[0;35m");
+}
