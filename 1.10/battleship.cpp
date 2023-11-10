220f5b9699d80c317fe64eb2f9843f407ce7e4e5
GannonGuess <gannonkguess@gmail.com>
2023-11-10 01:02:27 -0600

    1.10 personal project setup
    * created some files for personal 1.10 project

---
+++ 1.10/battleship.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <vector>
+#include "pair.h"
+
+class Ship {
+    public:
+        int length;
+        pair_t head;
+        pair_t tail;
+};
+
+class map {
+    public:
+        int height;
+        int width;
+        std::vector<Ship> ships;
+};
+
+void initilize_map() {
+    
+}
+
+
+int main(int argc, char *argv[]) {
+    
+    return 0;
+}
